8432a6f8723943aeab89747cff47ec17bb379697
nuca01 <nucaberiashvili16@gmail.com>
2024-07-26 17:10:44 +0400

    catch internet connection errors

--- Sources/NetworkingService/NetworkingService.swift
+++ Sources/NetworkingService/NetworkingService.swift
@@ -2,15 +2,16 @@
 // https://docs.swift.org/swift-book
 import Foundation
 
-//MARK: - enum NetworkError
+// MARK: - enum NetworkError
 public enum NetworkError: Error {
     case invalidURL
     case unexpectedStatusCode(String = "The status code returned was unexpected.")
     case unknown
     case decode
+    case noInternetConnection
 }
 
-//MARK: - class NetworkService
+// MARK: - class NetworkService
 public final class NetworkingService {
     public static var shared = NetworkingService()
     
@@ -19,12 +20,18 @@ public final class NetworkingService {
     public func sendRequest<T: Decodable>(endpoint: EndPoint, resultHandler: @escaping (Result<T, NetworkError>) -> Void) {
         
         guard let urlRequest = createRequest(endPoint: endpoint) else {
+            resultHandler(.failure(.invalidURL))
             return
         }
         
         let urlTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            guard error == nil else {
-                resultHandler(.failure(.invalidURL))
+            if let error = error as? URLError {
+                switch error.code {
+                case .notConnectedToInternet, .networkConnectionLost:
+                    resultHandler(.failure(.noInternetConnection))
+                default:
+                    resultHandler(.failure(.invalidURL))
+                }
                 return
             }
             guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
@@ -45,7 +52,7 @@ public final class NetworkingService {
         urlTask.resume()
     }
     
-    //MARK: - private helper method
+    // MARK: - private helper method
     private func createRequest(endPoint: EndPoint) -> URLRequest? {
         var urlComponents = URLComponents()
         urlComponents.scheme = endPoint.scheme
